f0ca21f6a56de982c881cae6a2b8a58c036cd722
ABDULRAHMAN ALRAHMA <aalrahma@red.sa>
2022-02-10 21:43:22 +0300

    implemented an api that reads a csv of products list with their rating, and returns the maximum rated products

--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from io import StringIO
+from fastapi import FastAPI, File, UploadFile
+from csv import DictReader
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/product/top")
+async def get_top_product(file: bytes = File(...)):
+    csv_fo = StringIO(file.decode("utf-8"))
+    r = DictReader(csv_fo)
+    max_product = Product()
+    
+    for row in r:
+        if float(row['customer_average_rating']) > max_product.rating:
+            max_product.id = int(row['id'])
+            max_product.name = row['product_name']
+            max_product.rating = float(row['customer_average_rating'])
+
+    return {
+        "top_product": max_product.name,
+        "product_rating": max_product.rating
+        }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class Product:
+  def __init__(self, id: int = 1, name: str = "", rating: float = -1):
+    self.id = id
+    self.name = name
+    self.rating = rating
