7c174089828c34c03a36cf9d85b94d3e70377fe9
ABDULRAHMAN ALRAHMA <aalrahma@red.sa>
2022-02-12 14:18:21 +0300

    - added a separate thread (daemon- so that it gets killed when the main program get's killed) for message broker

--- app/main.py
+++ app/main.py
@@ -1,28 +1,42 @@
+from io import StringIO
 from os import environ
+from threading import Thread
 from dotenv import load_dotenv
 load_dotenv()
 
-from multiprocessing import connection
-from fastapi import FastAPI
+from fastapi import FastAPI, File
+from app.services import product_service
+
+# RestAPI
+app = FastAPI()
+
+@app.post("/product/top")
+async def get_top_product(file: bytes = File(...)):
+    csv_file = StringIO(file.decode("utf-8"))
+    res = product_service.process_top_product(csv_file)
+
+    return res
+
 from pika import BlockingConnection, ConnectionParameters, PlainCredentials
 from app.mq_client import initClient
 from app.mq_server import initServer
 
 # Setup message broker
-credentials = PlainCredentials(environ.get('MQ_USER'), environ.get('MQ_PASS'))
-parameters = ConnectionParameters(environ.get("MQ_URL"),
-                                       environ.get('MQ_PORT'),
-                                       '/',
-                                       credentials)
-connection = BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue=environ.get('MQ_TO_BE_PROCESSED_Q'))
-channel.queue_declare(queue=environ.get('MQ_PROCESSED_Q'))
+def start_consumer():
+    credentials = PlainCredentials(environ.get('MQ_USER'), environ.get('MQ_PASS'))
+    parameters = ConnectionParameters(environ.get("MQ_URL"),
+                                        environ.get('MQ_PORT'),
+                                        '/',
+                                        credentials)
+    connection = BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue=environ.get('MQ_TO_BE_PROCESSED_Q'))
+    channel.queue_declare(queue=environ.get('MQ_PROCESSED_Q'))
 
-# Message Based
-initServer(channel)
-initClient(channel)
-channel.start_consuming()
+    # Message Based
+    initServer(channel)
+    initClient(channel)
+    channel.start_consuming()
 
-# RestAPI
-app = FastAPI()
+consumer_thread = Thread(target=start_consumer, daemon=True)
+consumer_thread.start()
